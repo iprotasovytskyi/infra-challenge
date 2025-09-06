b78bb3d51a5620c4a5301aec07942fa535917d87
Ihor P <ihor.protasovytskyi@m0.xyz>
2025-09-06 20:21:05 +0200

    deploy - 3

--- greeter/main.tf
+++ greeter/main.tf
@@ -5,15 +5,13 @@ terraform {
     helm       = { source = "hashicorp/helm", version = "~> 2.12" }
   }
   backend "s3" {
-    bucket         = "hivemind-tf-state"
-    key            = "greeter/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "tf-lock-hivemind-dev"
-    encrypt        = true
-    profile        = "hivemind"
+    bucket       = "hivemind-tf-state"
+    key          = "greeter/terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
-
 
 # AWS & cluster targeting
 variable "region" {
@@ -62,7 +60,7 @@ variable "hello_tag" {
 }
 
 provider "aws" {
-  region = var.region
+  region  = var.region
 }
 
 data "aws_eks_cluster" "this" { name = var.cluster_name }
